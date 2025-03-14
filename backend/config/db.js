61bd0439e2199df3f41e8e2de41d0023d8d36d55
Rakesh-Kumar-M-M <rakesh883872@gmail.com>
2025-03-14 20:34:14 +0530

    newly

--- backend/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB =async ()=>{
-    await mongoose.connect('mongodb+srv://Rakeshkumar:rakesh2005m@cluster0.klhm6.mongodb.net/food-del').then(()=> console.log("DB connected"));
-}
