ab71e6e5225b708fbbce66fc59ea5baf96d35683
Rakesh-Kumar-M-M <rakesh883872@gmail.com>
2025-04-19 12:36:08 +0530

    WIP: saving changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import { Routes,Route } from 'react-router-dom'
-import Add from './pages/add/add'
-import List from './pages/list/list'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
 import Order from './pages/Orders/Order'
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
