1b69a9ca1a54446af8b8c4f195adddaa24068527
Muthu Rakesh <rakesh883872@gmail.com>
2026-01-28 22:58:19 +0530

    Fix case-sensitive import issue

--- backend/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose"
-export const connectDB=async()=>{
-    (await mongoose.connect('mongodb+srv://Rakesh:rakesh2005m@cluster0.m8l63.mongodb.net/food-del').then(()=>console.log("DB connected")));
-}
--- food-del/backend/controllers/ordercontroller.js
+++ food-del/backend/controllers/ordercontroller.js
@@ -4,7 +4,7 @@ import Stripe from "stripe"
 
 const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeorder=async(req,res)=>{
-    const frontend_url="http://localhost:5174"
+    const frontend_url="https://food-del-frontend-sxfh.onrender.com/"
     try{
         const neworder=new ordermodel({
             userId:req.body.userId,
